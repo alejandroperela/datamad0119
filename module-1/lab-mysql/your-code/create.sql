305bfd5107864d0e18ab8ae8d19d1053e0860adc
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-22 20:10:24 +0100

    tercero

--- module-1/lab-mysql/your-code/create.sql
+++ module-1/lab-mysql/your-code/create.sql
@@ -21,8 +21,9 @@ CREATE TABLE IF NOT EXISTS `mydb`.`customers` (
   `idCustomers` INT NOT NULL AUTO_INCREMENT,
   `CustomerID` VARCHAR(45) NOT NULL,
   `Name` VARCHAR(45) NULL,
-  `Phone` INT NULL,
+  `Phone` VARCHAR(200) NULL,
   `Email` VARCHAR(45) NULL,
+  `Address` VARCHAR(45) NULL,
   `City` VARCHAR(45) NULL,
   `State/province` VARCHAR(45) NULL,
   `Country` VARCHAR(45) NULL,
@@ -49,8 +50,8 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Invoices` (
   `idInvoices` INT NOT NULL AUTO_INCREMENT,
   `Number` INT NOT NULL,
   `Date` DATETIME NULL,
-  `Car` VARCHAR(45) NULL,
-  `Customer` VARCHAR(45) NULL,
+  `Car` INT NULL,
+  `Customer` INT NULL,
   `Salesperson_idSalesperson` INT NOT NULL,
   `Customers_idCustomers` INT NOT NULL,
   `Salesperson_StaffID` INT NOT NULL,
@@ -133,3 +134,35 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
+
+INSERT INTO Cars (`idCars`,`VIN`,`Manufacturer`,`Model`,`Year`,`Color`) 
+VALUES
+(0,'3K096I98581DHSNUP','Volkswagen','Tiguan',2019,'Blue'),
+(1,'ZM8G7BEUQZ97IH46V','Peugeot','Rifter',2019,'Red'),
+(2,'RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White'),
+(3,'HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver'),
+(4,'DAM41UDN3CHU2WVF6','Volvo','V60',2019,'Gray'),
+(5,'DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray')
+
+INSERT INTO Salesperson (`StaffID`,`Name`,`Store`)
+VALUES
+(00001,'Petey Cruiser','Madrid'),
+(00002,'Anna Sthesia','Barcelona'),
+(00003,'Paul Molive','Berlin'),
+(00004,'Gail Forcewind','Paris'),
+(00005,'Paige Turner','Mimia'),
+(00006,'Bob Frapples','Mexico City'),
+(00007,'Walter Melon','Amsterdam'),
+(00008,'Shonda Leer','São Paulo')
+
+INSERT INTO Invoices (`idInvoices`,`Number`,`Date`,`Car`,`Customer`,`Salesperson_idSalesperson`)
+VALUES
+(0,852399038,'2018-08-22',0,1,3),
+(1,731166526,'2018-12-31',3,0,5),
+(2,271135104,'2019-01-22',2,2,7)
+
+INSERT INTO customers (`idCustomers`,`CustomerID`,`Name`,`Phone`,`Email`,`Address`,`City`,`State/province`,`Country`,`Zip`)
+VALUES
+(0,10001,'Pablo Picasso','+34 636 17 63 82','-','Paseo de la Chopera, 14','Madrid','Madrid','Spain',28045)
+(1,20001,'Abraham Lincoln','+1 305 907 7086','-','120 SW 8th St','Miami','Florida','United States',33130)
+(2,30001,'Napoléon Bonaparte','+33 1 79 75 40 00','-','40 Rue du Colisée','Paris','Île-de-France','France',75008)
