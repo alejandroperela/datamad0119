ae733713959f0a0f63ae897f1ebd31fffe681db7
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-23 22:06:24 +0100

    Corrección CHALLENGE 1

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -2,26 +2,30 @@ CHALLENGE 1
 
 Step 1
 
-SELECT titles.title_id 'Title_ID', titleauthor.au_id 'Author_ID', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 'Royalty'
+SELECT titles.title_id AS Title_ID, titleauthor.au_id AS Author_ID, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS Royalty
 FROM publications.titleauthor titleauthor
 INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
 INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
 
 Step 2
 
-SELECT titles.title_id 'Title_ID', titleauthor.au_id 'Author_ID', SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) 'Royalties'
+SELECT titles.title_id AS Title_ID, titleauthor.au_id AS Author_ID, SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS Royalties
 FROM publications.titleauthor titleauthor
 INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
 INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
-GROUP BY titles.title_id, titleauthor.au_id
+GROUP BY Title_ID, Author_ID
 
 Step 3
 #En este paso, me muestra 3 autores, pero no me los ordena por Profits, y no sé por qué. He probado diferentes cosas pero no he conseguido nada.
 
-SELECT titleauthor.au_id 'Author_ID', SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) 'Profits'
+SELECT titleauthor.au_id AS Author_ID, SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS Profits
 FROM publications.titleauthor titleauthor
 INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
 INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
-GROUP BY titleauthor.au_id
-ORDER BY 'Profits' ASC
+GROUP BY Author_ID
+ORDER BY Profits DESC
 LIMIT 3
+
+CHALLENGE 2
+
+Step 1
