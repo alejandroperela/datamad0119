a8e2f10c88b95f29360df3ecd9d5a1c054259959
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-23 20:54:19 +0100

    Fifth commit

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -14,3 +14,14 @@ FROM publications.titleauthor titleauthor
 INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
 INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
 GROUP BY titles.title_id, titleauthor.au_id
+
+Step 3
+#En este paso, me muestra 3 autores, pero no me los ordena por Profits, y no sé por qué. He probado diferentes cosas pero no he conseguido nada.
+
+SELECT titleauthor.au_id 'Author_ID', SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) 'Profits'
+FROM publications.titleauthor titleauthor
+INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
+INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
+GROUP BY titleauthor.au_id
+ORDER BY 'Profits' ASC
+LIMIT 3
