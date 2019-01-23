b64d3ba6d73f8249f920d3c283a4d710082b6a50
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-23 22:29:14 +0100

    Challenge 2

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -26,6 +26,27 @@ GROUP BY Author_ID
 ORDER BY Profits DESC
 LIMIT 3
 
+
 CHALLENGE 2
 
 Step 1
+
+CREATE TEMPORARY TABLE royaltyprofits
+SELECT titles.title_id AS Title_ID, titleauthor.au_id AS Author_ID, (titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) AS Royalty
+FROM publications.titleauthor titleauthor
+INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
+INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
+
+Step 2
+
+SELECT Title_ID, Author_ID, SUM(Royalty) AS Royalties
+FROM royaltyprofits
+GROUP BY Title_ID, Author_ID
+
+Step 3
+
+SELECT Author_ID, SUM(Royalty) AS Profits
+FROM royaltyprofits
+GROUP BY Author_ID
+ORDER BY Profits DESC
+LIMIT 3
