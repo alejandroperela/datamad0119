de484898ae301760ea1540af56982946ec8f0ead
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-23 20:44:14 +0100

    Fourth commit

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -9,4 +9,8 @@ INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
 
 Step 2
 
-
+SELECT titles.title_id 'Title_ID', titleauthor.au_id 'Author_ID', SUM(titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100) 'Royalties'
+FROM publications.titleauthor titleauthor
+INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
+INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
+GROUP BY titles.title_id, titleauthor.au_id
