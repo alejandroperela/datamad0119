228dc2aefc2a663c85ebf811e76e2ec1928aae8c
Alejandro Perela <alejandro.perela.posada@gmail.com>
2019-01-23 19:52:07 +0100

    Third commit

--- module-1/lab-advanced-mysql/your-code/solutions.sql
+++ module-1/lab-advanced-mysql/your-code/solutions.sql
@@ -2,11 +2,10 @@ CHALLENGE 1
 
 Step 1
 
-SELECT titles.title_id 'Title ID', authors.au_id 'Author ID', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 'sales_royalty'
-FROM publications.authors authors
-INNER JOIN publications.titleauthor titleauthor ON authors.au_id = titleauthor.au_id
+SELECT titles.title_id 'Title_ID', titleauthor.au_id 'Author_ID', titles.price * sales.qty * titles.royalty / 100 * titleauthor.royaltyper / 100 'Royalty'
+FROM publications.titleauthor titleauthor
+INNER JOIN publications.sales sales ON titleauthor.title_id = sales.title_id
 INNER JOIN publications.titles titles ON titleauthor.title_id = titles.title_id
-INNER JOIN publications.sales sales ON titles.title_id = sales.title_id
 
 Step 2
 
